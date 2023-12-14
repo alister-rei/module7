87616f9da20f560c5ea518b55f9a258a0f5c6a14
aleksey <lhadkov@mail.ru>
2023-12-15 01:25:57 +0300

    update lesson serializers

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -5,6 +5,8 @@ from lessons.models import Lesson
 
 # Serializers define the API representation.
 class LessonSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Lesson
         fields = '__all__'
