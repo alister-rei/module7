ca1fbf06833b2cd96fd8a8552eeae70ddea5718a
aleksey <lhadkov@mail.ru>
2023-12-20 00:54:47 +0300

    update user views

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import User
+from users.permissions import IsStaff
 from users.serializers import UserSerializer, UserCreateSerializer
 
 
@@ -15,20 +17,24 @@ class UserListAPIView(generics.ListAPIView):
     """отвечает за отображение списка сущностей."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     """отвечает за отображение одной сущности."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     """отвечает за редактирование сущности."""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
     """отвечает за удаление сущности."""
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
