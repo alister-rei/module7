127e827d02db29e68c0f35f3286fcd29a05fc70b
aleksey <lhadkov@mail.ru>
2023-12-20 00:55:08 +0300

    update user urls add token

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
@@ -7,9 +8,11 @@ from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView,
 app_name = UsersConfig.name
 
 urlpatterns = [
-                  path('create/', UserCreateAPIView.as_view(), name='create'),
-                  path('', UserListAPIView.as_view(), name='list'),
-                  path('<int:pk>/', UserRetrieveAPIView.as_view(), name='view'),
-                  path('edit/<int:pk>/', UserUpdateAPIView.as_view(), name='edit'),
-                  path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete'),
-              ]
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('create/', UserCreateAPIView.as_view(), name='create'),
+    path('', UserListAPIView.as_view(), name='list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='view'),
+    path('edit/<int:pk>/', UserUpdateAPIView.as_view(), name='edit'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='delete'),
+]
