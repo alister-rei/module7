20a4a467f5409794cfbd497ed2362ac0967afadc
aleksey <lhadkov@mail.ru>
2023-12-20 00:55:17 +0300

    update user model

--- users/models.py
+++ users/models.py
@@ -1,19 +1,26 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
 
-    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    phone = models.CharField(max_length=12, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
     city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
-    #
+
+    roles = models.CharField(max_length=20, choices=UserRoles.choices, default=UserRoles.MEMBER)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
@@ -23,3 +30,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+# Создаем группу модераторов
+moderator_group, created = Group.objects.get_or_create(name='moderator')
