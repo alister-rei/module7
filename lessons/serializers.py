d8b397319bb8ebac27dc2142787ad968f585595f
aleksey <lhadkov@mail.ru>
2023-12-24 23:14:19 +0300

    update lessons serializers.py add validator

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 
 from lessons.models import Lesson
+from main.validators import use_site_link, VideoLinkValidator
+from users.serializers import UserShortSerializer
 
 
 # Serializers define the API representation.
 class LessonSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(validators=[use_site_link])
+    owner = UserShortSerializer(read_only=True)
 
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [VideoLinkValidator(field='video')]
