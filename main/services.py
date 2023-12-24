bf6605c89d8844bf6dadd35e246b3efc394f38ed
aleksey <lhadkov@mail.ru>
2023-12-24 23:08:30 +0300

    update services add mail sender

--- main/services.py
+++ main/services.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import Group, Permission
+from django.core.mail import send_mail
+
+from config import settings
 
 
 def is_member(user):
     return user.groups.filter(name='moderator').exists()
+
+
+def mail_sender(email):
+    send_mail(
+        subject='Course update',
+        message='Course update',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=email
+    )
