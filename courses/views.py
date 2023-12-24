a819925b044268deb5f719a14e7bcc58449549ba
aleksey <lhadkov@mail.ru>
2023-12-24 23:13:31 +0300

    update courses views add paginator and send mail perform update

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from config import settings
 from courses.models import Course
 from courses.serializers import CourseSerializer
-from main.services import is_member
+from main.paginators import CoursePaginator
+from main.services import mail_sender
+from subs.models import Sub
 from users.permissions import OwnerOrModerator, IsOwner, NotModerator
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = CoursePaginator
 
     def perform_create(self, serializer):
         new_lesson = serializer.save()
@@ -28,8 +33,18 @@ class CourseViewSet(viewsets.ModelViewSet):
             permission_classes = []
         return [permission() for permission in permission_classes]
 
-    def get_queryset(self):
-        # Возвращает только объекты, принадлежащие текущему пользователю
-        if is_member(self.request.user):
-            return self.queryset.all()
-        return self.queryset.filter(owner=self.request.user)
+    # def get_queryset(self):
+    #     # Возвращает только объекты, принадлежащие текущему пользователю
+    #     if is_member(self.request.user):
+    #         return self.queryset.all()
+    #     return self.queryset.filter(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
+        pk = self.kwargs.get('pk')
+        course = get_object_or_404(Course, pk=pk)
+        subs = Sub.objects.filter(course=course, is_active=True)
+        email = list(subs.values_list('user__email', flat=True))  #
+
+        mail_sender(email)
+        return Response('Message sent')
